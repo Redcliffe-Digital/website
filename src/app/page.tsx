1655185ee53ca8127ba20cc6ea482ce5fa8b7334
joemillington <joemillington@protonmail.com>
2026-06-01 22:51:11 +0100

    Replace data-hero abstract motif with a photo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ import { Section } from '@/components/Section'
 import { SectionHeading } from '@/components/SectionHeading'
 import { Button } from '@/components/Button'
 import { Badges } from '@/components/Badges'
-import { DataMotif } from '@/components/DataMotif'
 import { CaseStudyCard } from '@/components/CaseStudyCard'
 import { sortedCaseStudiesBefore, sortedCaseStudiesDb } from '@/content/case-studies'
 import { site } from '@/lib/site'
@@ -210,7 +209,16 @@ export default function HomePage() {
               </div>
             </div>
             <div className="fade-up hidden lg:block" aria-hidden="true">
-              <DataMotif className="h-auto w-full" />
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img
+                src="/images/hero-data.jpg"
+                alt=""
+                width={900}
+                height={1125}
+                className="border-hairline aspect-[4/5] w-full rounded-sm border object-cover"
+                loading="eager"
+                decoding="async"
+              />
             </div>
           </div>
         </Container>
